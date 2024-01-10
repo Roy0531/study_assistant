9776a8ff26eca10852f16e8872dc05dcc5c949dd
Roy53 <ryosuke.orito.53@gmial.com>
2024-01-09 23:50:40 -0600

    removed unused file

--- src/components/dashboard_cmp/Trend.tsx
+++ src/components/dashboard_cmp/Trend.tsx
@@ -1,62 +1,61 @@
-import { getMastery } from "@/actions/get-mastery-action";
-import { differenceInDays } from 'date-fns';
-import { useEffect, useState } from "react";
-import { FiTrendingDown, FiTrendingUp } from "react-icons/fi";
-import { MdOutlineTrendingFlat } from "react-icons/md";
+// import { getMastery } from "@/actions/get-mastery-action";
+// import { differenceInDays } from 'date-fns';
+// import { useEffect, useState } from "react";
+// import { FiTrendingDown, FiTrendingUp } from "react-icons/fi";
+// import { MdOutlineTrendingFlat } from "react-icons/md";
 
-type Props = {
-  deck_id: number | null;
-  range: 7 | 31;
-}
+// type Props = {
+//   deck_id: number | null;
+//   range: 7 | 31;
+// }
 
-export default function Trend({ deck_id, range }: Props) {
-  const [percentage, setPercentage] = useState(0);
-  useEffect(() => {
-    const masteryData = async () => {
-        const mastery = await getMastery({deck_id, range});
-        if (mastery !== null) {
-          const oneWeekAgo = new Date();
-          oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-          const oneMonthAgo = new Date();
-          oneMonthAgo.setDate(oneMonthAgo.getDate() - 31);
+// export default function Trend({ deck_id, range }: Props) {
+//   const [percentage, setPercentage] = useState(0);
+//   useEffect(() => {
+//     const masteryData = async () => {
+//         const mastery = await getMastery({deck_id, range});
+//         if (mastery !== null) {
+//           const oneWeekAgo = new Date();
+//           oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+//           const oneMonthAgo = new Date();
+//           oneMonthAgo.setDate(oneMonthAgo.getDate() - 31);
 
-          let closestWeekRecord = null;
-          let closestMonthRecord = null;
+//           let closestWeekRecord = null;
+//           let closestMonthRecord = null;
 
-          for (const record of mastery) {
-              const daysFromWeekAgo = Math.abs(differenceInDays(record.timestamp, oneWeekAgo));
-              const daysFromMonthAgo = Math.abs(differenceInDays(record.timestamp, oneMonthAgo));
+//           for (const record of mastery) {
+//               const daysFromWeekAgo = Math.abs(differenceInDays(record.timestamp, oneWeekAgo));
+//               const daysFromMonthAgo = Math.abs(differenceInDays(record.timestamp, oneMonthAgo));
 
-              if (closestWeekRecord === null || daysFromWeekAgo < differenceInDays(closestWeekRecord.timestamp, oneWeekAgo)) {
-                  closestWeekRecord = record;
-              }
+//               if (closestWeekRecord === null || daysFromWeekAgo < differenceInDays(closestWeekRecord.timestamp, oneWeekAgo)) {
+//                   closestWeekRecord = record;
+//               }
 
-              if (closestMonthRecord === null || daysFromMonthAgo < differenceInDays(closestMonthRecord.timestamp, oneMonthAgo)) {
-                  closestMonthRecord = record;
-              }
-          }
+//               if (closestMonthRecord === null || daysFromMonthAgo < differenceInDays(closestMonthRecord.timestamp, oneMonthAgo)) {
+//                   closestMonthRecord = record;
+//               }
+//           }
 
-          if (range === 7 && closestWeekRecord !== null) {
-              setPercentage(closestWeekRecord.mastery);
-          }
+//           if (range === 7 && closestWeekRecord !== null) {
+//               setPercentage(closestWeekRecord.mastery);
+//           }
 
-          if (range === 31 && closestMonthRecord !== null) {
-              setPercentage(closestMonthRecord.mastery);
-          }
-      }
-    }
-      
-    masteryData();
-  },[deck_id, range])
-  return (  
-    <>
-      <div className={`w-10 h-10 flex items-center justify-center border-2 rounded-full mb-1
-      ${trending === 'up' ? 'border-up' : trending === 'flat' ? 'border-flat' : 'border-down'}`}>
-          {trending === "up" && <FiTrendingUp className='w-6 h-6'/>}
-          {trending === "flat" && <MdOutlineTrendingFlat className='w-6 h-6'/>}
-          {trending === "down" && <FiTrendingDown className='w-6 h-6'/>}
-        </div>
-        <p className='text-[10px] mb-2'>(+ {percentage} % from the last {range})</p>
-    </>
-  )
-}
+//           if (range === 31 && closestMonthRecord !== null) {
+//               setPercentage(closestMonthRecord.mastery);
+//           }
+//       }
+//     }
+//     masteryData();
+//   },[deck_id, range])
+//   return (  
+//     <>
+//       <div className={`w-10 h-10 flex items-center justify-center border-2 rounded-full mb-1
+//       ${trending === 'up' ? 'border-up' : trending === 'flat' ? 'border-flat' : 'border-down'}`}>
+//           {trending === "up" && <FiTrendingUp className='w-6 h-6'/>}
+//           {trending === "flat" && <MdOutlineTrendingFlat className='w-6 h-6'/>}
+//           {trending === "down" && <FiTrendingDown className='w-6 h-6'/>}
+//         </div>
+//         <p className='text-[10px] mb-2'>(+ {percentage} % from the last {range})</p>
+//     </>
+//   )
+// }
